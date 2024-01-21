aea3c52973aa12ff2e1e506171503181a8cb6b35
Andrew Tran <andrewtranva@gmail.com>
2024-01-21 16:24:28 -0500

    Added 'CanvasQuiz' and 'CanvasQuizQuestion' shared types to backend

--- server/assets/types.ts
+++ server/assets/types.ts
@@ -22,9 +22,9 @@ type CanvasCourse = {
   grading_periods?: object[]; // GradingPeriod type not strictly defined
   grading_standard_id?: number;
   grade_passback_setting?: string;
-  created_at?: string;
-  start_at?: string;
-  end_at?: string;
+  created_at?: Date;
+  start_at?: Date;
+  end_at?: Date;
   locale?: string;
   enrollments?: Enrollment[];
   total_students?: number;
@@ -67,15 +67,15 @@ type Calendar = {
 type Term = {
   id?: number;
   name?: string;
-  start_at?: string;
-  end_at?: string;
+  start_at?: Date;
+  end_at?: Date;
 };
 
 type CourseProgress = {
   requirement_count?: number;
   requirement_completed_count?: number;
   next_requirement_url?: string;
-  completed_at?: string;
+  completed_at?: Date;
 };
 
 type Enrollment = {
@@ -87,4 +87,102 @@ type Enrollment = {
   limit_privileges_to_course_section: boolean;
 };
 
-export { CanvasCourse };
+type CanvasQuiz = {
+  id?: number;
+  title?: string;
+  html_url?: string;
+  mobile_url?: string;
+  preview_url?: string;
+  description?: string;
+  quiz_type?: "practice_quiz" | "assignment" | "graded_survey" | "survey";
+  assignment_group_id?: number;
+  time_limit?: number;
+  shuffle_answers?: boolean;
+  hide_results?: "always" | "until_after_last_attempt" | null;
+  show_correct_answers?: boolean;
+  show_correct_answers_last_attempt?: boolean;
+  show_correct_answers_at?: Date;
+  hide_correct_answers_at?: Date;
+  one_time_results?: boolean;
+  scoring_policy?: "keep_highest" | "keep_latest";
+  allowed_attempts?: number;
+  one_question_at_a_time?: boolean;
+  question_count?: number;
+  points_possible?: number;
+  cant_go_back?: boolean;
+  access_code?: string;
+  ip_filter?: string;
+  due_at?: Date;
+  lock_at?: Date;
+  unlock_at?: Date;
+  published?: boolean;
+  unpublishable?: boolean;
+  locked_for_user?: boolean;
+  lock_info?: LockInfo;
+  lock_explanation?: string;
+  speedgrader_url?: string;
+  quiz_extensions_url?: string;
+  permissions?: QuizPermissions;
+  all_dates?: AssignmentDate[];
+  version_number?: number;
+  question_types?: string[];
+  anonymous_submissions?: boolean;
+};
+
+type LockInfo = {
+  unlock_at?: Date;
+  missing_permission?: string;
+  asset_string: string;
+};
+
+type AssignmentDate = {
+  due_at: Date;
+  unlock_at: Date;
+  lock_at: Date;
+  base: boolean;
+};
+
+type QuizPermissions = {
+  read?: boolean;
+  submit?: boolean;
+  create?: boolean;
+  manage?: boolean;
+  read_statistics?: boolean;
+  review_grades?: boolean;
+  update?: boolean;
+};
+
+type CanvasQuizQuestion = {
+  id: number;
+  quiz_id: number;
+  position?: number;
+  question_name?: string;
+  question_type?: string;
+  question_text?: string;
+  points_possible?: number;
+  correct_comments?: string;
+  incorrect_comments?: string;
+  neutral_comments?: string;
+  answers?: CanvasQuizAnswer[];
+};
+
+type CanvasQuizAnswer = {
+  id?: number;
+  answer_text: string;
+  answer_weight: number;
+  answer_comments?: string;
+  text_after_answers?: string;
+  answer_match_left?: string;
+  answer_match_right?: string;
+  matching_answer_incorrect_matches?: string;
+  numerical_answer_type?: "exact_answer" | "range_answer" | "precision_answer";
+  exact?: number;
+  margin?: number;
+  approximate?: number;
+  precision?: number;
+  start?: number;
+  end?: number;
+  blank_id?: number;
+};
+
+export { CanvasCourse, CanvasQuiz, CanvasQuizQuestion };
