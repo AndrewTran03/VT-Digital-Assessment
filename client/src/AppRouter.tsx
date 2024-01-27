fe9692e99ad076f42cd8f8d07412b36d176c33df
Andrew Tran <andrewtranva@gmail.com>
2024-01-27 04:44:31 -0500

    Fixed syntax mistake in AppRouter for Home Component reference

--- client/src/AppRouter.tsx
+++ client/src/AppRouter.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/home";
+import Home from "./components/Home";
 import FileImport from "./components/FileImport";
 // import Test from "./components/Test";
 
