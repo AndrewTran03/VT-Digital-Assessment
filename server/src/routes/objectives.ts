f81ce49622eb13d557172ab1b2dc0464c5b131e8
Andrew Tran <andrewtranva@gmail.com>
2024-01-27 04:30:44 -0500

    Fixed formatting for specified file

--- server/src/routes/objectives.ts
+++ server/src/routes/objectives.ts
@@ -3,12 +3,12 @@ import express from "express";
 const router = express.Router();
 
 router.get("/api/objective", async (req, res) => {
-    // const data = req.body;
+  // const data = req.body;
 });
 
 router.post("/api/objective", async (req, res) => {
-    const data = req.body;
-    console.log(data);
+  const data = req.body;
+  console.log(data);
 });
 
 export { router as canvasObjectiveRouter };
