de7b39afa079c4923a4774fc2f15dba8c8eb72e7
Andrew Tran <andrewtranva@gmail.com>
2024-01-27 05:00:27 -0500

    Removed redundant code from AppRouter in frontend

--- client/src/AppRouter.tsx
+++ client/src/AppRouter.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import FileImport from "./components/FileImport";
-// import Test from "./components/Test";
 
 function AppRouter() {
   return (
@@ -9,7 +8,6 @@ function AppRouter() {
       <>
         <Router>
           <Routes>
-            {/* <Route path="/" element={<Test num={2} name={"Andrew"} />} /> */}
             <Route path="/" element={<Home />} />
             <Route path="/file_import" element={<FileImport />} />
           </Routes>
