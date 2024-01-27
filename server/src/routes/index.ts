4f8cd001244f3e5288d172c39cc3f9f967aace59
Andrew Tran <andrewtranva@gmail.com>
2024-01-27 04:29:55 -0500

    Added Canvas and Objective Routers to main Express App Router

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
 
 import { exampleRouter } from "./example";
+import { canvasDataRouter } from "./canvas";
+import { canvasObjectiveRouter } from "./objectives";
 
 const router = express.Router();
 
 router.use(exampleRouter); // TODO: Remove this one later
 // Add other local routers here...
+router.use(canvasDataRouter);
+router.use(canvasObjectiveRouter);
 
 export default router;
