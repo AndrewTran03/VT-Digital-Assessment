5a56f95112d96bf06a5814a60e80a1e254301829
Andrew Tran <andrewtranva@gmail.com>
2024-01-27 04:13:01 -0500

    Replaced conventional Vite loading page with a dedicated React page-router

---
+++ client/src/AppRouter.tsx
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/home";
+import FileImport from "./components/FileImport";
+// import Test from "./components/Test";
+
+function AppRouter() {
+  return (
+    <>
+      <>
+        <Router>
+          <Routes>
+            {/* <Route path="/" element={<Test num={2} name={"Andrew"} />} /> */}
+            <Route path="/" element={<Home />} />
+            <Route path="/file_import" element={<FileImport />} />
+          </Routes>
+        </Router>
+      </>
+    </>
+  );
+}
+
+export default AppRouter;
