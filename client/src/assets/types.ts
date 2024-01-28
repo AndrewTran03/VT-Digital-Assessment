54d47ac49c2e48388019b59ae40aa19c1a468871
Andrew Tran <andrewtranva@gmail.com>
2024-01-27 22:53:32 -0500

    Added 'CanvasQuizAnswer', 'CanvasQuizQuestion', 'CanvasQuizQuestionGroup', and 'CanvasQuizMap' types in the frontend as a way to maintain TS types of QuizQuestion results (as demonstrated and organized in the backend)

--- client/src/assets/types.ts
+++ client/src/assets/types.ts
@@ -32,4 +32,52 @@ type CourseObjective = {
   canvasObjective: string;
 };
 
-export { Test2, backendUrlBase, APIErrorResponse, CourseObjective };
+type CanvasQuizQuestion = {
+  id: number;
+  quiz_id: number;
+  position?: number;
+  question_name?: string;
+  question_type?: string;
+  question_text?: string;
+  points_possible?: number;
+  correct_comments?: string;
+  incorrect_comments?: string;
+  neutral_comments?: string;
+  answers?: CanvasQuizAnswer[];
+};
+
+type CanvasQuizAnswer = {
+  id?: number;
+  answer_text: string;
+  answer_weight: number;
+  answer_comments?: string;
+  text_after_answers?: string;
+  answer_match_left?: string;
+  answer_match_right?: string;
+  matching_answer_incorrect_matches?: string;
+  numerical_answer_type?: "exact_answer" | "range_answer" | "precision_answer";
+  exact?: number;
+  margin?: number;
+  approximate?: number;
+  precision?: number;
+  start?: number;
+  end?: number;
+  blank_id?: number;
+};
+
+type CanvasQuizQuestionGroup = {
+  quizId: number;
+  questions: CanvasQuizQuestion[];
+};
+
+type CanvasQuizMap = Map<number, CanvasQuizQuestionGroup[]>;
+
+export {
+  Test2,
+  backendUrlBase,
+  APIErrorResponse,
+  CourseObjective,
+  CanvasQuizQuestion,
+  CanvasQuizQuestionGroup,
+  CanvasQuizMap
+};
