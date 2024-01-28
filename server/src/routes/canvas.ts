ed00fb4addcde87db1452c5770dc55de472affde
Andrew Tran <andrewtranva@gmail.com>
2024-01-27 22:49:58 -0500

    Added the GET Canvas API endpoint (works not with 'Map' replacer function)

--- server/src/routes/canvas.ts
+++ server/src/routes/canvas.ts
@@ -1,18 +1,20 @@
 import express from "express";
 import log from "../utils/logger";
 import { APIErrorResponse } from "../../assets/types";
-import { fetchCanvasQuizData } from "../canvas_interact/canvas.api";
+import { fetchCanvasUserCourseData, fetchCanvasUserQuizData, checkQuizMapHelper } from "../canvas_interact/canvas.api";
+import { mapReplacer } from "../utils/json.helper";
 
 const router = express.Router();
 
 let index = 0;
 router.get("/api/canvas", async (_, res) => {
   try {
-    const canvasResults = await fetchCanvasQuizData();
-
+    const canvasUserCourseIds = await fetchCanvasUserCourseData();
+    const canvasQuizMap = await fetchCanvasUserQuizData(canvasUserCourseIds);
+    // checkQuizMapHelper(canvasQuizMap);
     index++;
     log.info(`END OF GET REQUEST #${index} ------------------------`);
-    return res.status(200).json(canvasResults);
+    return res.status(200).send(JSON.stringify(canvasQuizMap, mapReplacer, 2));
   } catch (err) {
     log.error("Error with interacting the Canvas API! Please try again!");
     const resErrBody: APIErrorResponse = {
