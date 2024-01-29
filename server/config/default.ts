76bb485906d34f1d948dd5c236bc3cb1ec09e882
Andrew Tran <andrewtranva@gmail.com>
2024-01-28 23:02:02 -0500

    Added a 'mongoDatabaseName' config to backend

--- server/config/default.ts
+++ server/config/default.ts
@@ -4,5 +4,6 @@ export default {
   backendServerUrl: `http://localhost:${process.env.BACKEND_PORT}`,
   canvasPublicApiToken: `${process.env.CANVAS_PUBLIC_API_TOKEN}`,
   logLevel: "trace",
-  mongoDatabaseUri: `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.DEPLOYMENT_NAME}.xqbvmpf.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`
+  mongoDatabaseUri: `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.DEPLOYMENT_NAME}.xqbvmpf.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`,
+  mongoDatabaseName: `${process.env.MONGO_DB_NAME}`
 };
