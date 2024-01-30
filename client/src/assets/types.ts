791bb49da433e51f2e410c8433e4d5cb06ae6e20
Andrew Tran <andrewtranva@gmail.com>
2024-01-30 17:55:33 -0500

    Updated frontend to support added backend code to convert Map object of Canvas Quiz Questions into an Array structure to be MongoDB compliant

--- client/src/assets/types.ts
+++ client/src/assets/types.ts
@@ -50,7 +50,15 @@ type CanvasQuizQuestion = {
   quiz_id: number;
   position?: number;
   question_name?: string;
-  question_type?: string;
+  question_type?:
+    | "multiple_choice_question"
+    | "essay_question"
+    | "true_false_question"
+    | "multiple_dropdrowns_question"
+    | "fill_in_multiple_blanks_question"
+    | "multiple_answers_question"
+    | "short_answer_question"
+    | "numerical_question";
   question_text?: string;
   points_possible?: number;
   correct_comments?: string;
@@ -59,23 +67,30 @@ type CanvasQuizQuestion = {
   answers?: CanvasQuizAnswer[];
 };
 
+// type CanvasQuizAnswer = {
+//   id?: number;
+//   answer_text: string;
+//   answer_weight: number;
+//   answer_comments?: string;
+//   text_after_answers?: string;
+//   answer_match_left?: string;
+//   answer_match_right?: string;
+//   matching_answer_incorrect_matches?: string;
+//   numerical_answer_type?: "exact_answer" | "range_answer" | "precision_answer";
+//   exact?: number;
+//   margin?: number;
+//   approximate?: number;
+//   precision?: number;
+//   start?: number;
+//   end?: number;
+//   blank_id?: number;
+// };
+
 type CanvasQuizAnswer = {
-  id?: number;
-  answer_text: string;
-  answer_weight: number;
-  answer_comments?: string;
-  text_after_answers?: string;
-  answer_match_left?: string;
-  answer_match_right?: string;
-  matching_answer_incorrect_matches?: string;
-  numerical_answer_type?: "exact_answer" | "range_answer" | "precision_answer";
-  exact?: number;
-  margin?: number;
-  approximate?: number;
-  precision?: number;
-  start?: number;
-  end?: number;
-  blank_id?: number;
+  weight: number;
+  migration_id: string;
+  id: number;
+  text: string;
 };
 
 type CanvasQuizQuestionGroup = {
@@ -92,6 +107,5 @@ export {
   CourseObjectiveBase,
   CourseObjective,
   CanvasQuizQuestion,
-  CanvasQuizQuestionGroup,
   CanvasQuizMap
 };
