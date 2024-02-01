d31e5daa2098a604f4accb9a374d5cdf9eee92c0
Andrew Tran <andrewtranva@gmail.com>
2024-02-01 02:09:37 -0500

    Added parsing logic and Integrated React Context-Provider for Canvas Quiz Questions and Learning Objectives

--- client/src/components/UserDashboard.tsx
+++ client/src/components/UserDashboard.tsx
@@ -1,38 +1,47 @@
-import { useState, useEffect } from "react";
-import { backendUrlBase } from "../assets/types";
+import { useEffect, useContext, FormEvent } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { backendUrlBase } from "../assets/types";
+import { CanvasQuizQuestionContext } from "../shared";
+import { parseCanvasQuizQuestionMongoDBDCollection } from "../shared/FrontendParser";
 
 const UserDashboard: React.FC = () => {
-  // const [data, setData] = useState<CanvasQuizMap | null>(null);
-
-  async function fetchCanvasQuizData() {
-    // await axios.get(`${backendUrlBase}/api/canvas`).then((res) => {
-    //   console.log(res);
-    // });
-
-    await axios.get(`${backendUrlBase}/api/objective`).then((res) => {
-      console.log(res);
-    });
-  }
-
-  async function handleSubmit() {
-    console.clear();
-    await fetchCanvasQuizData();
-  }
+  const { canvasQuizDataArr, setCanvasQuizDataArr } = useContext(CanvasQuizQuestionContext);
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetchCanvasQuizData();
   }, []);
 
-  // useEffect(() => {
-  //   console.log(data);
-  // }, [data]);
+  async function fetchCanvasQuizData() {
+    await axios.get(`${backendUrlBase}/api/canvas`).then((res) => {
+      const parsedResult = parseCanvasQuizQuestionMongoDBDCollection(res.data);
+      console.log(parsedResult);
+      setCanvasQuizDataArr(parsedResult);
+    });
+  }
+
+  async function handleAPIButtonClick(e: FormEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    console.clear();
+    await fetchCanvasQuizData();
+  }
+
+  function handleClickToMatcher(e: FormEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    console.clear();
+    // Hard-coded for now (will change later)
+    navigate("/learning_obj_match", { state: { canvasCourseInternalId: 185413, quizId: 171111 } });
+  }
 
   return (
     <>
       <p>Table Goes Here</p>
-      <button type="submit" onClick={handleSubmit}>
+      <button type="submit" onClick={handleAPIButtonClick}>
         Refresh
+      </button>
+      <button type="submit" onClick={handleClickToMatcher}>
+        Click here to go Learning Objective Matcher
       </button>
     </>
   );
