1388b4e706e2fff161e762209bc27d4bc2ea0924
Andrew Tran <andrewtranva@gmail.com>
2024-02-01 01:29:16 -0500

    Reconfigured ''@typescript-eslint/no-explicit-any' property to 'warn' instead of 'error'

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -15,6 +15,7 @@ module.exports = {
       { allowConstantExport: true },
     ],
     "@typescript-eslint/no-unused-vars": "warn",
-    "@typescript-eslint/ban-types": "warn"
+    "@typescript-eslint/ban-types": "warn",
+    "@typescript-eslint/no-explicit-any": "warn"
   },
 }
