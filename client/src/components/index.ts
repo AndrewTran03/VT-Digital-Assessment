5b60433c3de3572d88871022152c1a4af92697fb
Andrew Tran <andrewtranva@gmail.com>
2024-02-15 10:20:51 -0500

    Combined imports for all React frontned components to one exportable file

---
+++ client/src/components/index.ts
@@ -0,0 +1,6 @@
+import FileImport from "./FileImport";
+import LearningObjectiveMatcher from "./LearningObjectiveMatcher";
+import UserDashboard from "./UserDashboard";
+import QuizStatistics from "./QuizStatistics";
+
+export { FileImport, LearningObjectiveMatcher, UserDashboard, QuizStatistics };
