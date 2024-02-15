2939656b9a7bd823ad7085b2b1b65deae14867c5
Andrew Tran <andrewtranva@gmail.com>
2024-02-15 00:47:08 -0500

    Fixed AppRouter to include all contexts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import "./App.css";
 import AppRouter from "./AppRouter";
-import { CanvasCourseQuizMongoDBEntry, LearningObjectiveQuizData } from "./shared/types";
-import { CanvasQuizQuestionContext, LearningObjectiveContext } from "./shared";
+import { CanvasCourseQuizMongoDBEntry, LearningObjectiveQuizData, CanvasCourseAssociations } from "./shared/types";
+import { CanvasQuizQuestionContext, LearningObjectiveContext, CanvasUserIdContext } from "./shared/contexts";
+import CanvasUserCourseNamesContext from "./shared/contexts/CanvasUserCourseNamesContext";
 
 const App: React.FC = () => {
   // Initial States of Each React-Context's Shared Data
@@ -11,11 +12,17 @@ const App: React.FC = () => {
     canvasCourseInternalId: 0,
     quizId: 0
   });
+  const [canvasUserId, setCanvasUserId] = useState(0);
+  const [canvasUserCourseNamesArr, setCanvasUserCourseNamesArr] = useState<CanvasCourseAssociations[]>([]);
 
   return (
     <CanvasQuizQuestionContext.Provider value={{ canvasQuizDataArr, setCanvasQuizDataArr }}>
       <LearningObjectiveContext.Provider value={{ canvasLearningObjectiveData, setCanvasLearningObjectiveData }}>
-        <AppRouter />
+        <CanvasUserIdContext.Provider value={{ canvasUserId, setCanvasUserId }}>
+          <CanvasUserCourseNamesContext.Provider value={{ canvasUserCourseNamesArr, setCanvasUserCourseNamesArr }}>
+            <AppRouter />
+          </CanvasUserCourseNamesContext.Provider>
+        </CanvasUserIdContext.Provider>
       </LearningObjectiveContext.Provider>
     </CanvasQuizQuestionContext.Provider>
   );
