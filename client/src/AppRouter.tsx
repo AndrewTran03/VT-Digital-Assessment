26abc1adb7e2f3e6a32916439c714cbe0851c59f
Andrew Tran <andrewtranva@gmail.com>
2024-02-15 10:20:22 -0500

    Created a basic Quiz Statistics component skeleton

--- client/src/AppRouter.tsx
+++ client/src/AppRouter.tsx
@@ -1,7 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import FileImport from "./components/FileImport";
-import UserDashboard from "./components/UserDashboard";
-import LearningObjectiveMatcher from "./components/LearningObjectiveMatcher";
+import { FileImport, LearningObjectiveMatcher, UserDashboard, QuizStatistics } from "./components";
 
 const AppRouter: React.FC = () => {
   return (
@@ -11,6 +9,7 @@ const AppRouter: React.FC = () => {
           <Route path="/" element={<UserDashboard />} />
           <Route path="/file_import" element={<FileImport />} />
           <Route path="/learning_obj_match" element={<LearningObjectiveMatcher />} />
+          <Route path="/statistics" element={<QuizStatistics />} />
         </Routes>
       </Router>
     </>
---
+++ client/src/components/QuizStatistics.tsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+
+type Props = {};
+
+const QuizStatistics: React.FC<Props> = (props) => {
+  const [name, setName] = useState("");
+
+  return <></>;
+};
+
+export default QuizStatistics;
