3be9136bc264cc8edffcc2381c4db13b212d8262
Andrew Tran <andrewtranva@gmail.com>
2024-02-15 00:45:12 -0500

    Combined all contexts into one singular 'index.ts' file (easier for exporting) across frontend application

---
+++ client/src/shared/contexts/index.ts
@@ -0,0 +1,6 @@
+import CanvasQuizQuestionContext from "./CanvasQuizQuestionContext";
+import LearningObjectiveContext from "./LearningObjectiveContext";
+import CanvasUserIdContext from "./CanvasUserId";
+import CanvasUserCourseNamesArrContext from "./CanvasUserCourseNamesContext";
+
+export { CanvasQuizQuestionContext, LearningObjectiveContext, CanvasUserIdContext, CanvasUserCourseNamesArrContext };
