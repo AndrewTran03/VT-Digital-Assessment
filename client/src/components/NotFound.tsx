5b16699d5ff488853501e95305e50f393732c39b
Andrew Tran <andrewtranva@gmail.com>
2024-02-17 23:43:42 -0500

    Added React routing fallback logic with this new NotFound component

--- client/src/components/NotFound.tsx
+++ client/src/components/NotFound.tsx
@@ -1,18 +1,24 @@
 import { useEffect, useState } from "react";
-import { UserDashboard } from "./index";
+import { useCookies } from "react-cookie";
 import { Navigate } from "react-router-dom";
 
 const NotFound: React.FC = () => {
+  const [, setAuthenticated] = useState(false);
   const [alertShown, setAlertShown] = useState(false);
-  
+  const [cookies] = useCookies(["Authenticated"]);
+
   useEffect(() => {
-    if (!alertShown) {
+    const isAuthenticated = cookies.Authenticated as boolean;
+    setAuthenticated(isAuthenticated);
+
+    if (!alertShown && !isAuthenticated) {
       alert("Invalid route!");
       setAlertShown(true);
+      window.localStorage.setItem("invalidPath", location.pathname);
     }
-  }, [alertShown]);
+  }, [alertShown, cookies.Authenticated]);
 
-  return <Navigate to="/dashboard"/>;
+  return <Navigate to="/" />;
 };
 
 export default NotFound;
