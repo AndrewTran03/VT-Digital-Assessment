e7808b8c20fd077fa6b31cf9ad68017d3a90ddf5
Andrew Tran <andrewtranva@gmail.com>
2024-02-18 00:02:31 -0500

    Added a helper function to return a correct AuthHeaders for extrnal backend Canvas API Axios calls based on passed-in Canvas User Id parameter (will fallback to my API token if it is not already exists in the MongoDB database by the time of this function's invokation)

--- server/src/utils/canvas.connection.ts
+++ server/src/utils/canvas.connection.ts
@@ -1,9 +1,18 @@
 import log from "./logger";
-import { fetchCanvasUserInfo } from "../canvas_interact/canvas.api";
+import config from "config";
+import { fetchCanvasUserInfoRegUser } from "../canvas_interact/canvas.api";
+import { CanvasUserApiModel } from "../models/canvas.user.api";
+import { AxiosAuthHeaders } from "../../assets/types";
+
+const fallbackCanvasApiKey = config.get<string>("canvasPublicApiToken"); // Andrew Tran's API token (fallback)
 
 async function ensureConnectionToCanvasApi() {
+  const axiosHeaders: AxiosAuthHeaders = {
+    Authorization: `Bearer ${fallbackCanvasApiKey}`
+  };
+
   try {
-    const canvasUserId = await fetchCanvasUserInfo();
+    const canvasUserId = await fetchCanvasUserInfoRegUser(axiosHeaders);
     if (canvasUserId == -1) {
       throw new Error("Failed to connect to Canvas API. Exiting now...");
     }
@@ -15,4 +24,27 @@ async function ensureConnectionToCanvasApi() {
   }
 }
 
-export { ensureConnectionToCanvasApi };
+async function getCanvasApiAuthHeaders(canvasUserId: number) {
+  let usedCanvasAPIKey = "";
+  try {
+    if (canvasUserId === -1) {
+      throw new Error("Invalid Canvas User ID...not attempting to search MongoDB database");
+    }
+    const canvasUserApiKeyFindResult = await CanvasUserApiModel.findOne({ canvasUserId: canvasUserId });
+    if (!canvasUserApiKeyFindResult) {
+      throw new Error("Invalid search for Canvas User Information within MongoDB database");
+    }
+    usedCanvasAPIKey = canvasUserApiKeyFindResult.canvasUserApiKey;
+  } catch (err) {
+    log.error((err as Error).message);
+    usedCanvasAPIKey = fallbackCanvasApiKey;
+  }
+  
+  const axiosHeaders: AxiosAuthHeaders = {
+    Authorization: `Bearer ${usedCanvasAPIKey}`
+  };
+  
+  return axiosHeaders;
+}
+
+export { ensureConnectionToCanvasApi, getCanvasApiAuthHeaders };
