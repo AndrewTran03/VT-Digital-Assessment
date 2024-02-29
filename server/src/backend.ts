1da93552952090733f878488d46ebac3611e86cb
Andrew Tran <andrewtranva@gmail.com>
2024-02-29 10:31:17 -0500

    Added 'socket.io' integration to the backend

--- server/src/backend.ts
+++ server/src/backend.ts
@@ -5,6 +5,8 @@ import express, { NextFunction } from "express";
 import bodyParser from "body-parser";
 import config from "config";
 import cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
 import log from "./utils/logger";
 import router from "./routes";
 import { ensureConnectionToCanvasApi } from "./utils/canvas.connection";
@@ -27,8 +29,26 @@ app.use((_, res, next: NextFunction) => {
 const backendServerPort = config.get<number>("backendServerPort");
 const backendServerUrl = config.get<string>("backendServerUrl");
 
-app.listen(backendServerPort, async () => {
-  log.info(`App started on ${backendServerUrl}`);
+// Socket.io Setup (Server):
+const server = http.createServer(app);
+const ioSocket = new Server(server, {
+  cors: {
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD"]
+  }
+});
+
+// Tracks incoming connections to SocketIO server connection
+ioSocket.on("connection", (socket) => {
+  log.debug(`User sucessfully connected to Socket.io!\nSERVER-SIDE #ID: ${socket.id}`);
+
+  socket.conn.on("close", (reason) => {
+    log.debug(`A user disconnected. REASON: ${reason}`);
+  });
+});
+
+server.listen(backendServerPort, async () => {
+  log.info(`Server started on ${backendServerUrl}`);
   await ensureConnectionToCanvasApi();
   await ensureConnectionToMongoDatabase();
 });
