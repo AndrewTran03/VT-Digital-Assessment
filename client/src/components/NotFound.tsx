8bea7e59219202f41a2e24025d9fd044a0dcda85
Andrew Tran <andrewtranva@gmail.com>
2024-03-24 18:54:05 -0400

    Fixed the logic of the 'NotFound' component

--- client/src/components/NotFound.tsx
+++ client/src/components/NotFound.tsx
@@ -1,23 +1,8 @@
-import { useEffect, useState } from "react";
-import { useCookies } from "react-cookie";
 import { Navigate } from "react-router-dom";
 
 const NotFound: React.FC = () => {
-  const [, setAuthenticated] = useState(false);
-  const [alertShown, setAlertShown] = useState(false);
-  const [cookies] = useCookies(["Authenticated"]);
-
-  useEffect(() => {
-    const isAuthenticated = cookies.Authenticated as boolean;
-    setAuthenticated(isAuthenticated);
-
-    if (!alertShown && !isAuthenticated) {
-      alert("Invalid route!");
-      setAlertShown(true);
-      window.localStorage.setItem("invalidPath", window.location.pathname);
-    }
-  }, [alertShown, cookies.Authenticated]);
-
+  window.alert("Invalid route!");
+  window.localStorage.setItem("invalidPath", window.location.pathname);
   return <Navigate to="/" />;
 };
 
