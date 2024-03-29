3731a8bc3e91f68c81191dd5aaa71165e1e95fab
Andrew Tran <andrewtranva@gmail.com>
2024-03-29 13:50:31 -0400

    Fixed frontend development/production enviornments by better utilizing the Linux Bash Script better

--- client/run_frontend.sh
+++ client/run_frontend.sh
@@ -1,9 +1,43 @@
-# !/bin/bash
+#!/bin/bash
 
 echo "|-------- Beginning of Running Front-End Script --------|"
 echo "Installing NPM Dependencies"
 time npm install
 echo "Running React + Vite Frontend:"
 npm run format
-npm run dev
+
+# Function to display usage instructions
+display_usage() {
+    echo "Usage: $0 <mode>"
+    echo "<mode> should be 'development' (or 'd') or 'production' (or 'p')"
+}
+
+# Check if the script was called with an argument
+if [ $# -eq 0 ]; then
+    # No argument provided, prompt the user for input
+    echo "Please type [ 'development' or 'd' ] or [ 'production' or 'p' ]:"
+    read mode 
+else
+    # Use the first argument passed to the script
+    mode="$1"
+fi
+
+modeLowercase=$(echo "$mode" | tr '[:upper:]' '[:lower:]')
+# Error-checking loop while the 1-argument input is not valid
+while [ "$modeLowercase" != "development" ] && [ "$modeLowercase" != "d" ] && [ "$modeLowercase" != "production" ] && [ "$modeLowercase" != "p" ]; do
+    echo "Invalid mode selected: '$mode'."
+    display_usage
+    read mode
+    modeLowercase=$(echo "$mode" | tr '[:upper:]' '[:lower:]')
+done
+
+# Check if the mode is 'development' or 'production'
+if [ "$modeLowercase" == "development" ] || [ "$modeLowercase" == "d" ]; then
+    echo "Frontend: Development mode selected"
+    npm run dev
+elif [ "$modeLowercase" == "production" ] || [ "$modeLowercase" == "p" ]; then
+    echo "Frontend: Production mode selected"
+    npm run preview -- --host
+fi
+
 echo "|-------- End of Running Front-End Script --------|"
